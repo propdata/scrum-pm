9465eaf0a0bd53c1076cdaff31c593f2db988561
Greg Armer <greg@propdata.net>
2012-05-13 00:25:23 -0400

    Setup correct CI test db details

--- scrum_pm/scrum_pm/settings.py
+++ scrum_pm/scrum_pm/settings.py
@@ -13,9 +13,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'spm',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'USER': 'test',                      # Not used with sqlite3.
+        'PASSWORD': 'test',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
